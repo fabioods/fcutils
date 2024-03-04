44524229b905c51970e629a2346fe42f4ba99a77
Fábio dos Santos <fah_ds@live.com>
2024-03-04 19:20:40 -0300

    Add methods to manipule events

--- pkg/events/event_dispatcher.go
+++ pkg/events/event_dispatcher.go
@@ -26,3 +26,41 @@ func (e *EventDispatcher) Register(eventName string, handler EventHandlerInterfa
 	e.handlers[eventName] = append(e.handlers[eventName], handler)
 	return nil
 }
+
+func (e *EventDispatcher) Dispatch(event EventInterface) error {
+	for _, handler := range e.handlers[event.GetName()] {
+		err := handler.Handle(event)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (e *EventDispatcher) Remove(eventName string, handler EventHandlerInterface) error {
+	if _, ok := e.handlers[eventName]; ok {
+		for i, h := range e.handlers[eventName] {
+			if h == handler {
+				e.handlers[eventName] = append(e.handlers[eventName][:i], e.handlers[eventName][i+1:]...)
+				return nil
+			}
+		}
+	}
+	return nil
+}
+
+func (e *EventDispatcher) Has(eventName string, handler EventHandlerInterface) bool {
+	if _, ok := e.handlers[eventName]; ok {
+		for _, h := range e.handlers[eventName] {
+			if h == handler {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (e *EventDispatcher) Clear() error {
+	e.handlers = make(map[string][]EventHandlerInterface)
+	return nil
+}
